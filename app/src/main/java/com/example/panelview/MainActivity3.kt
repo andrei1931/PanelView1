04db8a1ef61039b9af1743e5fba0c777e1507540
andrei1931 <andreipasca904@gmail.com>
2023-11-16 22:00:29 +0200

    varianta de inceput

--- app/src/main/java/com/example/panelview/MainActivity3.kt
+++ app/src/main/java/com/example/panelview/MainActivity3.kt
@@ -1,7 +1,6 @@
 package com.example.panelview
 
 import android.content.ContentValues.TAG
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,58 +9,56 @@ import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+class MainActivity3 : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        auth = FirebaseAuth.getInstance()
+        setContentView(R.layout.activity_main3)
+
         val buton: Button = findViewById(R.id.button)
         val user_name: EditText = findViewById(R.id.editTextTextPersonName2)
         val parola: EditText = findViewById(R.id.editTextTextPassword2)
-        val buton2: Button = findViewById(R.id.button2)
-
-        buton.setOnClickListener {
+        auth=FirebaseAuth.getInstance()
+        buton.setOnClickListener() {
             val usr = user_name.text.toString()
             val pwd = parola.text.toString()
-            aut(usr, pwd)
-        }
-        buton2.setOnClickListener {
-            Intent(this, MainActivity3::class.java).also {
-                startActivity(it)
-            }
+            reg(usr, pwd)
+
         }
     }
 
-
-    fun aut(usr: String, pwd: String) {
+    fun reg(usr: String, pwd: String) {
         if (usr.isEmpty() || pwd.isEmpty()) {
             val myToast =
                 Toast.makeText(applicationContext, "Introdu emailul și parola", Toast.LENGTH_SHORT)
             myToast.show()
-        } else {
-            auth.signInWithEmailAndPassword(usr, pwd)
+        }
+        else{
+            auth.createUserWithEmailAndPassword(usr, pwd)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
+                        Log.d(TAG, "createUserWithEmail:success")
+                        Toast.makeText(
+                            baseContext,
+                            "Inregistrare reusita.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
                         val user = auth.currentUser
-                        Intent(this, MainActivity2::class.java).also {
-                            startActivity(it)
-
-                        }
 
                     } else {
                         // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
                         Toast.makeText(
                             baseContext,
-                            "Email sau parola gresita",
+                            "Inregistrare esuata.",
                             Toast.LENGTH_SHORT,
                         ).show()
 
                     }
                 }
+
+
         }
     }
 }
